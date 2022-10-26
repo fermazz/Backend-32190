74d2775b02f9dc15e1c3105fb5166533f913b755
Fernando Mazzucco <fer_mazz@hotmail.com>
2022-10-25 23:19:24 -0300

    Desafio 1

---
+++ Desafio1/desafio1.js
@@ -0,0 +1,62 @@
+class Usuario {
+
+    constructor (nombre, apellido, libros, mascotas) {
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+        
+    }
+
+    getFullName(){
+        return `Nombre completo del usuario es: ${this.nombre} ${this.apellido}`
+    };
+
+    addMascotas(nombreMascota){
+        this.mascotas.push(nombreMascota)
+    };
+
+    countMascotas(){
+        return `Número de mascotas que tiene el usuario es: ${this.mascotas.length}`
+    }
+
+    addBock(nombreLibro, autorLibro){
+        this.libros.push({
+            titulo: nombreLibro,
+            autor: autorLibro
+        })
+    }
+
+    getBookNames(){
+        return [libros.nombreLibro]
+    }
+
+}
+
+let usuario = new Usuario ('Fernando', 'Mazzucco', [
+    {
+        titulo: 'El hobbit',
+        autor: 'J.R.R. Tolkien',
+    },
+    {
+        titulo: 'El alquimista',
+        autor: 'Paulo Coelho'
+    },
+    {
+        titulo: 'La  metamorfosis',
+        auto: 'Franz Kafka', 
+    },
+], ['Fido', 'Preto', 'Pichu']);
+
+console.log(usuario);
+console.log(usuario.getFullName());
+
+usuario.addMascotas('Pandi');
+
+console.log(usuario.countMascotas());
+
+usuario.addBock("De la tierra a la luna", "Julio Verne")
+
+
+
