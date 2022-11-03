b15b5a64f2b6fef6834ddcbb65cd2d60bd765844
Fernando Mazzucco <fer_mazz@hotmail.com>
2022-11-03 18:02:38 -0300

    Desafio 2

--- Desafio2/desafio2.js
+++ Desafio2/desafio2.js
@@ -17,7 +17,7 @@ class Contenedor {
     }
 
     const newObj = {...obj, newId}
-    obj.push(newObj)
+    objs.push(newObj)
 
     try {
 
@@ -31,13 +31,20 @@ class Contenedor {
 
 //getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
 
-  async getById(num) {
+  async getById(id) {
 
-    const objs = await this.getAll()
-
+    
     try {
-      let item 
+      const objs = await this.getAll()
+      const obj = objs.find((obj) => obj.id === id)
+      if(!obj) {
+        console.log('null')
+      }else{
+        console.log('Producto con id ', id, obj)
+      }
+
     } catch (error) {
+      throw new Error ('no hay productos con ese id')
       
     }
   }
@@ -59,13 +66,30 @@ class Contenedor {
 
 //deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
 
-  async deleteById() {}
+  async deleteById(id) {
+    try {
+      const objs = await this.getAll()
+      // verifico si existe el id, si no mando error
+      const obj = objs.find((obj) => obj.id === id)
+      if (!obj) {
+        throw new Error ("No fue encontrado el producot con el id ")
+      }
+      // filtro el array de productos y mequedo con los que no tengan el id que quiero eliminar
+      const newObjs = objs.filter((obj) => obj.id !== id)
+      await fs.writeFile(this.ruta, JSON.stringify(newObjs, null, 2))
+
+    } catch (error) {
+      console.log("Error en deleteById: ", error)
+
+
+    }
+  }
 
 //deleteAll(): void - Elimina todos los objetos presentes en el archivo.
 
   async deleteAll() {
     try {
-      await fs.writeFile(this.ruta, [])
+      await fs.writeFile(this.ruta, JSON.stringify([], null, 2))
       console.log('Archivo borrado')
 
     } catch (error) {
@@ -75,29 +99,41 @@ class Contenedor {
   }
 }
 
-const nuevoArchivo = new Contenedor('')
 
-const p1 = {                                                                                                                                                    
+const main = async () => {
+const contenedor = new Contenedor('productos.txt')
+
+await contenedor.save({
+                                                                                                                                                    
     title: 'Escuadra',                                                                                                                                 
     price: 123.45,                                                                                                                                     
     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',                                     
                                                                                                                                                   
-  } 
+})
 
-  const p2 = {                                                                                                                                                    
+await contenedor.save({                                                                                                                                                   
     title: 'Calculadora',                                                                                                                              
     price: 234.56,                                                                                                                                     
     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',                                          
                                                                                                                                                  
-  }
+  })
 
-  const p3 ={                                                                                                                                                    
+await contenedor.save({                                                                                                                                                    
     title: 'Globo Terráqueo',                                                                                                                          
     price: 345.67,                                                                                                                                     
     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',                                   
                                                                                                                                                   
-  }      
+  })    
 
+  const id = 3
+  await contenedor.getById(id)
+  const objs = await contenedor.getAll()
+  console.log('array de productos ', objs)
+  
+  
+}
+
+main ()
 //   nuevoArchivo.save(p1)
 
 // const contenedor1 = new Contenedor('/.productos.txt')
