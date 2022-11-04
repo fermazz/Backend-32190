214eee9248659fc57589689d3c620fe403aa62df
Fernando Mazzucco <fer_mazz@hotmail.com>
2022-11-03 22:50:32 -0300

    Desafio 2

--- Desafio2/desafio2.js
+++ Desafio2/desafio2.js
@@ -1,72 +1,73 @@
-const {promises: fs} = require("fs");
+const {
+  promises: fs
+} = require("fs");
 
 class Contenedor {
   constructor(ruta) {
     this.ruta = ruta
   }
 
-//getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
+  //getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
 
-async getAll() {
+  async getAll() {
 
-  try {
-    // ??? aqui va this.ruta o '/.Productos.txt'
-      const objs = await fs.readFile(this.ruta, 'utf-8') 
+    try {
+      // ??? aqui va this.ruta o '/.Productos.txt'
+      const objs = await fs.readFile(this.ruta, 'utf-8')
       return JSON.parse(objs)
-      
-  } catch (error) {
-     return []
-      
-  }
-}
 
-//save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-  async save(obj) {
+    } catch (error) {
+      return []
 
-    const objs = await this.getAll()
-    let newId 
-    if (objs.length == 0){
-        newId = 1
-    }else{
-        newId = objs[objs.length -1].id +1
     }
+  }
 
-    const newObj = {...obj, newId}
-    objs.push(newObj)
+  //save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+  async save(obj) {
 
     try {
 
-        await fs.writeFile(this.ruta, JSON.stringify(objs, null, 2))
-        
+      const objs = await this.getAll()
+
+      // Busco el último id para generar un nuevo id sumandole 1
+      const lastId = objs[objs.length - 1].id
+      const newObj = {...obj, id: lastId + 1}
+      objs.push(newObj)
+
+      await fs.writeFile(this.ruta, JSON.stringify(objs, null, 2))
+      console.log('Se creo un nuevo producto con id: ' + newObj.id)
+
     } catch (error) {
-        throw new Error (`Error al guardar ${err}`)
-        
+
+      // Si no existe el archivo, creamos por primera vez la lista de productos
+      const newObj = {...obj, id: 1}
+      await fs.writeFile(this.ruta, JSON.stringify([newObj], null, 2))
+      console.log("Se creo nuevo producto con id: " + newObj.id)
+
     }
   }
 
-//getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
+  //getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
 
   async getById(id) {
 
-    
+
     try {
       const objs = await this.getAll()
       const obj = objs.find((obj) => obj.id === id)
-      if(!obj) {
+      if (!obj) {
         console.log('null')
-      }else{
+      } else {
         console.log('Producto con id ', id, obj)
       }
 
     } catch (error) {
-      throw new Error ('no hay productos con ese id')
-      
+      throw new Error('no hay productos con ese id')
+
     }
   }
 
-
-
-//deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
+  //deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
 
   async deleteById(id) {
     try {
@@ -74,7 +75,7 @@ async getAll() {
       // verifico si existe el id, si no mando error
       const obj = objs.find((obj) => obj.id === id)
       if (!obj) {
-        throw new Error ("No fue encontrado el producot con el id ")
+        throw new Error("No fue encontrado el producot con el id ")
       }
       // filtro el array de productos y mequedo con los que no tengan el id que quiero eliminar
       const newObjs = objs.filter((obj) => obj.id !== id)
@@ -82,12 +83,10 @@ async getAll() {
 
     } catch (error) {
       console.log("Error en deleteById: ", error)
-
-
     }
   }
 
-//deleteAll(): void - Elimina todos los objetos presentes en el archivo.
+  //deleteAll(): void - Elimina todos los objetos presentes en el archivo.
 
   async deleteAll() {
     try {
@@ -97,48 +96,42 @@ async getAll() {
     } catch (error) {
       throw new Error `Error al borrar el archivo ${error}`
     }
-    
   }
 }
 
 
 const main = async () => {
-const contenedor = new Contenedor('productos.txt')
+  const contenedor = new Contenedor('productos.txt')
 
-await contenedor.save({
-                                                                                                                                                    
-    title: 'Escuadra',                                                                                                                                 
-    price: 123.45,                                                                                                                                     
-    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',                                     
-                                                                                                                                                  
-})
+  await contenedor.save({
 
-await contenedor.save({                                                                                                                                                   
-    title: 'Calculadora',                                                                                                                              
-    price: 234.56,                                                                                                                                     
-    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',                                          
-                                                                                                                                                 
+    title: 'Escuadra',
+    price: 123.45,
+    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+
   })
 
-await contenedor.save({                                                                                                                                                    
-    title: 'Globo Terráqueo',                                                                                                                          
-    price: 345.67,                                                                                                                                     
-    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',                                   
-                                                                                                                                                  
-  })    
+  await contenedor.save({
+    title: 'Calculadora',
+    price: 234.56,
+    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
+
+  })
+
+  await contenedor.save({
+    title: 'Globo Terráqueo',
+    price: 345.67,
+    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
+
+  })
 
   const id = 3
-  await contenedor.getById(id)
+  await contenedor.getById(2)
   const objs = await contenedor.getAll()
   console.log('array de productos ', objs)
   // await contenedor.deleteById(2)
   // await contenedor.deleteAll()
 
-  
-  
 }
 
-main ()
-
-
-
+main()
